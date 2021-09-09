05249623b280b93f2d30ece687098ab3244418f1
jdicen <jonathan@webstepup.com.au>
2021-09-09 15:44:19 +0800

    Services Page

---
+++ pages/automate-your-business.js
@@ -0,0 +1,74 @@
+
+import fetcher from "../lib/fetcher";
+import { GET_AUTOMATE_BUSINESS_PAGE } from "../lib/wordpress/api";
+import { FOOTER_DATA } from "../lib/wordpress/api";
+import Layout from "./components/layout";
+import styles from "../styles/Automate.module.css";
+
+const content = ({pageContent,footerContent}) => {
+    const elements = pageContent;
+    const footerData = footerContent;
+
+    return(
+        <Layout elements={elements,footerData}>
+            <section className="pt-8 pb-10">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 order-sm-first order-last vmiddle">
+                            <h2 className={styles.automateHeading}>{elements.automateBusiness.automate1Heading}</h2>
+                            <br/><br/>
+                            <div dangerouslySetInnerHTML={{__html: elements.automateBusiness.automate1Description}} />   
+                        </div>
+                        <div className="col-md-6">
+                            <div className={styles.crmImg}>
+                                <img src={elements.automateBusiness.automate1Image.sourceUrl} width="60%"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.mtsm2+" container mt-8"}>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className={styles.botImg}>
+                                <img src={elements.automateBusiness.automate2Image.sourceUrl} width="60%"/>
+                            </div>
+                        </div>
+                        <div className="col-md-6 vmiddle">
+                            <h2 className={styles.automateHeading}>{elements.automateBusiness.automate2Heading}</h2>
+                            <br/><br/>
+                            <div dangerouslySetInnerHTML={{__html: elements.automateBusiness.automate2Description}} />   
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.mtsm2+" container mt-8"}>
+                    <div className="row">
+                        <div className="col-md-6 order-sm-first order-last vmiddle">
+                            <h2 className={styles.automateHeading}>{elements.automateBusiness.automate3Heading}</h2>
+                            <br/><br/>
+                            <div dangerouslySetInnerHTML={{__html: elements.automateBusiness.automate3Description}} />   
+                        </div>
+                        <div className="col-md-6">
+                            <div className={styles.webImg}>
+                                <img src={elements.automateBusiness.automate3Image.sourceUrl} width="60%"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Layout>
+    );    
+
+}    
+
+export default content;
+
+export async function getStaticProps(){
+    const response = await fetcher(GET_AUTOMATE_BUSINESS_PAGE);
+    const footerResponse = await fetcher(FOOTER_DATA);
+    const pageContent = response.data.page;
+    const footerContent = footerResponse.data.page;
+    return{
+        props: {pageContent,footerContent},
+        revalidate: 1,
+    };
+  }
