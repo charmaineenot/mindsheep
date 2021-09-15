5744c2a4a0f14c65b157e3638058d9e8b9322243
Charmaine Rose Enot <charmaineenot@gmail.com>
2021-09-15 15:42:33 +0800

    Fixed single blog post page

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,17 +1,19 @@
 import fetcher from "../../lib/fetcher";
 import { GET_ALL_POSTS_WITH_SLUG, GET_POST_BY_SLUG } from "../../lib/wordpress/api";
+import { FOOTER_DATA } from '../../lib/wordpress/api';
 import { useRouter } from "next/router";
 import Layout from "../../components/layout";
 
-const post = ({postData}) => {
+const post = ({postData,footerContent}) => {
     const blogPost = postData.data.post;
+    const footerData = footerContent;
     const router = useRouter;
 
     if(!router.isFallback && !blogPost?.slug){
         return <ErrorPage status = {404}/>
     }
     return(
-        <Layout>
+        <Layout elements={blogPost,footerData}>
             <div className="container">
                 <div className="row">
                     <div className="col-md-12"></div>
@@ -32,7 +34,9 @@ export default post;
 
 export async function getStaticPaths(){
     const response = await fetcher(GET_ALL_POSTS_WITH_SLUG);
+    
     const allPosts = await response.data.posts.nodes;
+    
 
     return {
         paths: allPosts.map((post) => `/post/${post.slug}`) || [],
@@ -41,15 +45,18 @@ export async function getStaticPaths(){
 }
 
 export async function getStaticProps({params}){
-     const variables = {
-         id: params.slug,
-         idType: "SLUG",
-     };
+    const footerResponse = await fetcher(FOOTER_DATA);
+    const footerContent = footerResponse.data.page;
+    const variables = {
+        id: params.slug,
+        idType: "SLUG",
+    };
 
      const data = await fetcher(GET_POST_BY_SLUG, {variables});
      return {
          props: {
              postData: data,
+             footerContent
          }
      }
 }
