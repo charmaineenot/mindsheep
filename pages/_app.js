99d3cf012cd8fca1a9986c5567bca7af38d54942
Charmaine Rose Enot <charmaineenot@gmail.com>
2021-10-11 14:43:14 +0800

    Removed bootstrap.min from app.js

--- pages/_app.js
+++ pages/_app.js
@@ -43,11 +43,11 @@ function MyApp({ Component, pageProps }) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link async defer href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;600&display=swap" rel="stylesheet" />
     //  bootstrap CDN
-    <link
+    {/* <link
     strategy="lazyOnload" async href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
     crossorigin="anonymous" 
-    />
+    /> */}
     <script
     strategy="lazyOnload" async src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
