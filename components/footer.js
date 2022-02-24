377527fe1fee244c681ed04b61acecaedc7b7e95
drevenmacx <macalolotdreven@gmail.com>
2022-02-24 15:16:34 +0800

    Mobile Footer Changes

--- components/footer.js
+++ components/footer.js
@@ -20,7 +20,7 @@ const Footer = ({elements}) => (
   <div className="Footer">
     <section className="align-items-center partners" style={footerStyle}>
       {/* Partners Section */}
-      <div className="container-fluid pt-4 pb-4 bg-darken">
+      <div className="container-fluid pt-4 pb-4 p-5 bg-darken">
         <div className="boxed">
           <div className="row text-center d-flex align-items-center">
             <div className="col-lg-3 col-md-3 col-6 pt-4 pb-4">
@@ -74,7 +74,7 @@ const Footer = ({elements}) => (
         </div>
       </div>
     </section>
-    <section className="align-items-center footerlogo" style={footerStyle}> 
+    <section className="align-items-center footerlogo homeSec" style={footerStyle}> 
       {/* Main Footer */}
       <div className="container-fluid p-0">
         <div className="h-75p bg-darken"></div>
@@ -110,16 +110,70 @@ const Footer = ({elements}) => (
           </div>
           <div class="col-lg-5 col-md-4 vmiddle text-center">
             <p className="text-black mb-0 d-none">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <span className="text-orange me-2">
-              <a href="https://www.facebook.com/mindsheepmarketing/" className="text-orange text-decoration-none"><FontAwesomeIcon icon={faFacebookSquare} size="3x"></FontAwesomeIcon></a>
+            <span className="text-grey me-2">
+              <a href="https://www.facebook.com/mindsheepmarketing/" className="text-grey text-decoration-none"><FontAwesomeIcon icon={faFacebookSquare} size="3x"></FontAwesomeIcon></a>
             </span>
-            <span className="text-orange">
-              <a href="https://www.instagram.com/mindsheepmarketing/" target="_blank" className="text-orange text-decoration-none"><FontAwesomeIcon icon={faInstagramSquare} size="3x"></FontAwesomeIcon></a>
+            <span className="text-grey">
+              <a href="https://www.instagram.com/mindsheepmarketing/" target="_blank" className="text-grey text-decoration-none"><FontAwesomeIcon icon={faInstagramSquare} size="3x"></FontAwesomeIcon></a>
             </span>
           </div>
         </div>
       </div>
     </section>
+
+    <section className="align-items-center footerlogo homeDesk" style={footerStyle}> 
+      {/* Mobile Footer */}
+      <div className="container-fluid p-0">
+        <div className="h-75p bg-darken"></div>
+      </div>
+      <div className="container-fluid p-3 pb-0 bg-orange text-center text-sm-start">
+        <div className="boxed">
+          <div className="row">
+              <div class="col-6 leftMindsheep">
+              <Image width="405" height="104" src="https://wp.mindsheep.com.au/wp-content/uploads/2021/10/Footer-Logo-01-Min-401x105-1.webp" />
+              <p class="mt-2">We are a company of bold and strategic thinkers — a diverse team with a deep desire to create sustainable marketing and lead generating systems, ultimately generating wealth for our clients.</p>
+            </div>
+            <div class="col-6 pRight">
+              <ul class="pMind">
+                <li><a className="text-white" href="/who-we-are">Who We Are</a></li>
+                <li><a className="text-white" href="/our-approach">Our Approach</a></li>      
+                <li><a className="text-white" href="/lead-generation">Lead Generation</a></li>        
+                <li><a className="text-white" href="/automate-your-business">Automate Your Business</a></li>
+                <li><a className="text-white" href="/brand-building">Build Your Brand</a></li>
+              </ul>
+            </div>
+            <div className="col-md-4">
+              <div className="footerSheepCont">
+                <Image width="320" height="311" src="https://wp.mindsheep.com.au/wp-content/uploads/2021/10/Footer-Sheep-01-Mim-321x312-1.webp" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container p-3 text-center text-sm-start"> 
+        <div className="row">
+          <div class="col-lg-7 col-md-8 vmiddle">
+            <NewsletterSubscribe/>
+          </div>
+          <div class="col-lg-5 col-md-4 vmiddle text-center">
+            <p className="text-black mb-0 d-none">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <span className="text-grey me-2">
+              <a href="https://www.facebook.com/mindsheepmarketing/" className="text-grey text-decoration-none"><FontAwesomeIcon icon={faFacebookSquare} size="3x"></FontAwesomeIcon></a>
+            </span>
+            <span className="text-grey">
+              <a href="https://www.instagram.com/mindsheepmarketing/" target="_blank" className="text-grey text-decoration-none"><FontAwesomeIcon icon={faInstagramSquare} size="3x"></FontAwesomeIcon></a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    
+
+
+
     <section className="pt-3 pb-3 bg-orange align-items-center text-white text-center text-sm-start">
       <div className="container p-0">
         <div className="row rowAuto">
