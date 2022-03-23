985e96bf695930b3c7d77ec37033b4c50defc993
Charmaine Rose Enot <charmaineenot@gmail.com>
2022-03-23 11:20:42 +0800

    Updated facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -24,15 +24,15 @@ function MyApp({ Component, pageProps }) {
     };
   }, [router.events]);
 
-  useEffect(async () => {
-    const { default: ReactPixel } = await import('react-facebook-pixel');
-    ReactPixel.init('1083310298772388', null, {
-        autoConfig: true,
-        debug: true,
-      });
-    ReactPixel.pageView();
-    ReactPixel.track("ViewContent")
-  });
+  // useEffect(async () => {
+  //   const { default: ReactPixel } = await import('react-facebook-pixel');
+  //   ReactPixel.init('1083310298772388', null, {
+  //       autoConfig: true,
+  //       debug: true,
+  //     });
+  //   ReactPixel.pageView();
+  //   ReactPixel.track("ViewContent")
+  // });
 
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap.bundle")
@@ -81,6 +81,25 @@ function MyApp({ Component, pageProps }) {
         `,
       }}
     />
+    <script strategy="lazyOnload"
+      dangerouslySetInnerHTML={{
+        __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1083310298772388');
+        fbq('track', 'PageView');
+        `,
+      }}
+    />
+    <noscript><img height="1" width="1" style={{display:"none"}}
+    src="https://www.facebook.com/tr?id=1083310298772388&ev=PageView&noscript=1"
+    /></noscript>
     </Head>
     <Component {...pageProps} />
     </>
