f0b17e4b1bd15fa42f1c286d335bacee47510d7f
Latryll Alimes <latrylizer@gmail.com>
2023-05-23 09:03:36 +0800

    GTM update

--- components/gtm-noscript.js
+++ components/gtm-noscript.js
@@ -5,7 +5,7 @@ export default function gtmNoscript() {
     <noscript
       dangerouslySetInnerHTML={{
         __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3CZ2MV"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`
       }}
     ></noscript>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -144,10 +144,10 @@ function MyApp({ Component, pageProps }) {
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K3CZ2MV');`,
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K3CZ2MV');`,
           }}
         ></script>
         <noscript>
